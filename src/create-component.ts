033e48ee4926d9ec5511f92de50f3b6aa10def36
Anton Platonov <platosha@gmail.com>
2022-10-19 10:54:50 +0300

    cleanup: improve Polymer workaround

--- src/create-component.ts
+++ src/create-component.ts
@@ -1,12 +1,17 @@
 import { createComponent as createComponentOriginal, EventName } from '@lit-labs/react';
 
 export function createComponent(...args: Parameters<typeof createComponentOriginal>) {
-  // TODO: improve or remove the Polymer workaround
-  // 'createComponent' relies on key presence on the custom element class, but
-  // Polymer defines properties on the prototype when the first element
-  // is created. Workaround, create a throw-away element to make the prototype
-  // reliable for 'createComponent'.
-  const [, tagName] = args;
-  document.createElement(tagName);
+  const elementClass = args[2];
+  if ('_properties' in elementClass) {
+    // TODO: improve or remove the Polymer workaround
+    // 'createComponent' relies on key presence on the custom element class,
+    // but Polymer defines properties on the prototype when the first element
+    // is created. Workaround: pass a mock object with properties in
+    // the prototype.
+    args[2] = {
+      name: elementClass.name,
+      prototype: (elementClass as any)._properties,
+    } as typeof elementClass;
+  }
   return createComponentOriginal(...args);
 }
