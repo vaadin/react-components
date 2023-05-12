0ad040b2787b1afc584e26d5c817093fcc90a22a
Anton Platonov <platosha@gmail.com>
2023-05-12 09:46:30 +0300

    chore: improve formatting

--- src/Crud.ts
+++ src/Crud.ts
@@ -3,9 +3,9 @@ export * from './generated/Crud.js';
 /**
  * A helper function that allows declaring the value path (or key) on
  * the fields in a custom form of a `<Crud />` component.
- * 
+ *
  * ### Usage
- * 
+ *
  * ```tsx
  * <Crud>
  *   <FormLayout slot="form">
@@ -13,12 +13,12 @@ export * from './generated/Crud.js';
  *   </FormLayout>
  * </Crud>
  * ```
- * 
+ *
  * @param path The path of the particular field's value in the CRUD
  * item objects.
- * 
+ *
  * @returns object with HTML attribute values recognized on a CRUD form field.
  */
 export function crudPath(path: string): Record<string, string> {
-  return {path} as Record<string, string>;
+  return { path } as Record<string, string>;
 }
