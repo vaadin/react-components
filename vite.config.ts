65cc18d201977ba2c4fc09c59b4187f840b17dff
Vlad Rindevich <vladrin@vaadin.com>
2023-06-15 12:28:22 +0300

    fix: use verbatimModuleSyntax TS option

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,6 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
-  // @ts-expect-error: Unknown error "typeof import(@vitejs/plugin-react) has
-  // no call signatures". Not sure why it happens.
   plugins: [react()],
   root: resolve(process.cwd(), 'test/kitchen-sink'),
 });
