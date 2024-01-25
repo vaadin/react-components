bb5af368e09794888662009c43552205041c8674
Tomi Virkki <virkki@vaadin.com>
2024-01-25 16:39:23 +0200

    update version script

--- scripts/package-json-version.ts
+++ scripts/package-json-version.ts
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from 'node:fs/promises';
+import { readFile, writeFile, readdir } from 'node:fs/promises';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import type { PackageJson } from 'type-fest';
@@ -6,34 +6,42 @@ import type { PackageJson } from 'type-fest';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const rootDir = resolve(__dirname, '..');
 
-// TODO: Iterate for root and packages
 console.log('Updating Vaadin web components versions in "package.json"...');
 
-const packageJsonPath = resolve(rootDir, 'package.json');
-const packageJson = JSON.parse(await readFile(packageJsonPath, 'utf8')) as PackageJson;
+const workspacePackageJsonPath = resolve(rootDir, 'package.json');
+const workspacePackageJson = JSON.parse(await readFile(workspacePackageJsonPath, 'utf8')) as PackageJson;
 // Web components version should be the same as the version of the react-components package:
-const componentsVersion = packageJson.version;
+const componentsVersion = workspacePackageJson.version;
 console.log(`Using "${componentsVersion}" as the version for web components.`);
 
-Object.keys(packageJson.dependencies!).forEach((name) => {
-  if (!name.startsWith('@vaadin/')) {
-    return;
-  }
+const packagesDir = resolve(rootDir, 'packages');
+const packages = await readdir(packagesDir);
 
-  packageJson.dependencies![name] = componentsVersion;
-});
+for (const packageName of packages) {
+  const packageJsonPath = resolve(packagesDir, packageName, 'package.json');
+  const packageJson = JSON.parse(await readFile(packageJsonPath, 'utf8')) as PackageJson;
 
-await writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8');
+  Object.keys(packageJson.dependencies!).forEach((name) => {
+    if (!name.startsWith('@vaadin/')) {
+      return;
+    }
+
+    packageJson.version = componentsVersion;
+    packageJson.dependencies![name] = componentsVersion;
+  });
+
+  await writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8');
+}
 
 // Update version metadata in the sources
 
-// const versionSourceFile = 'src/utils/createComponent.ts';
-// console.log(`Updating version metadata in "${versionSourceFile}"...`);
-// const versionSourcePath = resolve(rootDir, versionSourceFile);
-// const versionSource = await readFile(versionSourcePath, 'utf8');
-// const newVersionSource = versionSource.replace(
-//   /version:.+,/g,
-//   `version: /* updated-by-script */ '${packageJson.version}',`,
-// );
+const versionSourceFile = 'packages/react-components/src/utils/createComponent.ts';
+console.log(`Updating version metadata in "${versionSourceFile}"...`);
+const versionSourcePath = resolve(rootDir, versionSourceFile);
+const versionSource = await readFile(versionSourcePath, 'utf8');
+const newVersionSource = versionSource.replace(
+  /version:.+,/g,
+  `version: /* updated-by-script */ '${componentsVersion}',`,
+);
 
-// await writeFile(versionSourcePath, newVersionSource, 'utf8');
+await writeFile(versionSourcePath, newVersionSource, 'utf8');
