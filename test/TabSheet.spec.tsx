dfc64bed5ec6b727826dc66ea083509749ec71bb
Tatu Lund <tatu@vaadin.com>
2025-07-30 11:22:17 +0300

    Update TabSheet.spec.tsx

--- test/TabSheet.spec.tsx
+++ test/TabSheet.spec.tsx
@@ -168,7 +168,7 @@ describe('TabSheet', () => {
     const content = getTabSheet().querySelector('#content-div');
     const rect = content?.getBoundingClientRect();
 
-    expect(rect?.width).toBe('600px');
-    expect(rect?.height).toBe('450px');
+    expect(rect?.width).toBe(600);
+    expect(rect?.height).toBe(450);
   });
 });
